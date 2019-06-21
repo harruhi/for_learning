e64097dc2c47ab09e1c8b1514d6a69844f4744a2
harruhi <51483322+harruhi@users.noreply.github.com>
2019-06-22 00:26:04 +0900

    Add files via upload

---
+++ what did you like.py
@@ -0,0 +1,7 @@
+something_like = ["이어폰, 가족, 노래, 펜, 책, 코딩"]
+something_like_you = [f"{str(input("당신이 좋아하는 것은?"))}"]
+
+something_like_you.append = str(input("hoxy?"))
+
+print(f"그럼 우선 제가먼저 좋아하는것을 알려드릴게요!{something_like}", end = "이게 제가 좋아하는 것들이에요!")
+print(f"당신이 좋아하는것은 {something_likeyou}", end = "군요!")
